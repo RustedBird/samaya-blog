51621683c3531a89830ce79a01934e275c79dda8
RustedBird <vlasov.31189@gmail.com>
2018-08-23 14:18:42 +0300

    Main page

---
+++ index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<section class="container-fluid main-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-12 offset-sm-0 text-center main-logo">
+                <img src="img/mainLogo.png" alt="main logo">
+            </div>
+        </div>
+        <div class="row logo-row">
+            <div class="col-lg-4 logo-item">
+                <a href="#">
+                    <img src="img/blogLogo.png" alt="blog Logo">
+                    <div class="desc-wrapper">
+                        <p class="logo-description">Советы. Опыт.
+                            Лучшее из лучшего для девушек.</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 logo-item">
+                <a href="#">
+                    <img src="img/instLogo.png" alt="inst Logo">
+                    <div class="desc-wrapper">
+                        <p class="logo-description">Ты самая красивая в Инстаграм. За 3 дня преобрази свой профиль,
+                            начни привлекать подписчиков и
+                            клиентов без профессиональных фото, сложных сервисов и тех навыков. Даже если сейчас у тебя
+                            меньше
+                            1к подписчиков!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 logo-item ">
+                <a href="#">
+                    <img src="img/ckuLogo.png" alt="cku Logo">
+                    <div class="desc-wrapper">
+                        <p class="logo-description">Закрытое сообщество для резидентов. Ещё больше опыта, знаний,
+                            возможностей</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row logo-row">
+            <div class="col-lg-4 offset-lg-2 logo-item">
+                <a href="#">
+                    <img src="img/schoolLogo.png" alt="school Logo">
+                    <div class="desc-wrapper">
+                        <p class="logo-description">Научись создавать ее для других!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 logo-item">
+                <a href="#">
+                    <img src="img/travelLogo.png" alt="travel Logo">
+                    <div class="desc-wrapper">
+                        <p class="logo-description">Здесь мы создаем жизнь,<br>
+                            которая нас достойна</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
